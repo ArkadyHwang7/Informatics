9c3285c543261ed4ad861422864b82e53667f520
ArkadyHwang7 <arkadijhvan456@gmail.com>
2025-10-02 22:43:08 +0300

    Add files via upload

---
+++ Dz1/InformaticDZ.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    float x, y;
+    cout << "Введите x и y: ";
+    cin >> x >> y;
+
+    float R = (-x - sqrt(exp(2.0f) - 4.0f * x * y)) / (2.0f * x);
+    float S = log(x) * tan(y);
+
+    cout << "R = " << R << endl;
+    cout << "S = " << S << endl;
+
+    float C = fmax(R, S);
+
+    cout << "Максимальное значение C = " << C << "\n";
+
+    cout << "\nНажмите Enter, чтобы выйти...";
+    cin.ignore();   
+    cin.get();      
+}
